ec163f6ed74818ccec68e872e7553a203ece4f15
Serhii Kotsiuba <s-p-ko@ukr.net>
2021-11-02 17:53:32 +0200

    Added addTodo inside App.tsx

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,7 +3,26 @@ import {useState, useEffect, useRef} from 'react';
 import {ITodo} from '../types/data';
 
 const App: React.FC = ()=> {
-  return <div />
+  const [value, setValue] = useState('');
+  const [todos, setTodos] = useState<ITodo[]>([]);
+
+  const addTodo = () => {
+    if (value) {
+      setTodos([...todos, {
+        id: Date.now(),
+        title: value,
+        complete: false,
+      }])
+      setValue('');
+    }
+  }
+  
+  return <div>
+    <div>
+      <input value={value} onChange={e=>setValue(e.target.value)} />
+      <button onClick={addTodo}>Add</button>
+    </div>
+  </div>
 }
 
 export {App}
