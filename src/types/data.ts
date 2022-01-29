2e906ce342ca4c57dfa9d32d6c978d70b051fdd1
Serhii Kotsiuba <s-p-ko@ukr.net>
2022-01-29 20:20:45 +0200

    Remove ITodoListProps, ITodoItem into data.ts

--- src/types/data.ts
+++ src/types/data.ts
@@ -3,3 +3,14 @@ export interface ITodo{
   title: string;
   complete: boolean;
 }
+
+export interface ITodoListProps {
+  items: ITodo[];
+  removeTodo: (id: string) => void;
+  toggleTodo: (id: string) => void;
+}
+
+export interface ITodoItem extends ITodo {
+  removeTodo: (id: string) => void;
+  toggleTodo: (id: string) => void;
+}
